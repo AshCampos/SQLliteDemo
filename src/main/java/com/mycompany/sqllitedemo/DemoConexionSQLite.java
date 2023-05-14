5fda0777089ade70e63ee681d37260947829d4f7
Tarea <ashlycampos00@aragon.unam.mx>
2023-05-14 17:10:37 -0600

    se agrega el proyecto

--- src/main/java/com/mycompany/sqllitedemo/DemoConexionSQLite.java
+++ src/main/java/com/mycompany/sqllitedemo/DemoConexionSQLite.java
@@ -1,13 +1,33 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.mycompany.sqllitedemo;
 
-/**
- *
- * @author Luis Angel
- */
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import org.sqlite.JDBC;
+
 public class DemoConexionSQLite {
-    
+
+    public static void main(String[] args) throws SQLException {
+        String DB_URL = "jdbc:sqlite:librosDB.db";
+        Connection conexion = null;
+
+        try {
+            Class.forName("org.sqlite.JDBC");
+            conexion = DriverManager.getConnection(DB_URL);
+            Statement stm = conexion.createStatement();
+            ResultSet rst = stm.executeQuery("SELECT * FROM libros; ");
+            while (rst.next()){
+                System.out.println(rst.getString(1) + " " + rst.getString(2));
+            }
+            conexion.close();
+        } catch (ClassNotFoundException cne) {
+            cne.printStackTrace();
+        } catch (SQLException sqle) {
+            sqle.printStackTrace();
+        }
+
+    }
+
 }
